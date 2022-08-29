46290fcc3694a2348954ca7a203559c050fb0ecb
Sylvan Martin <alephnulltim@gmail.com>
2022-08-29 14:50:06 -0400

    Initial Commit

---
+++ Pretty Stuff/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pretty Stuff
+//
+//  Created by Sylvan Martin on 8/29/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Pretty StuffUITests/Pretty_StuffUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pretty_StuffUITestsLaunchTests.swift
+//  Pretty StuffUITests
+//
+//  Created by Sylvan Martin on 8/29/22.
+//
+
+import XCTest
+
+class Pretty_StuffUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
